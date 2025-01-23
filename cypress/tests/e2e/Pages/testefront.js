9aa6fa9dd7265a2fcba0f8c4ccea219414b6e8ea
Thiago Marques <thiago.rodriguesmarque@gmail.com>
2025-01-23 17:06:08 -0300

    Desenvolvimento do cenário 02 web

--- cypress/tests/e2e/Pages/testefront.js
+++ cypress/tests/e2e/Pages/testefront.js
@@ -1,4 +1,5 @@
 import testeapi from "../../api/Pages/testeapi";
+import { faker } from '@faker-js/faker';
 
 class TestFront {
     acessarPagina() {
@@ -15,6 +16,29 @@ class TestFront {
 
     entaoOPainelPrincipalEVisualizado(){
         cy.url().should('include', '/admin/home');
+    }
+
+    fazerOCadastroDeUsuario(){
+        this.acessarPaginaDeCadatro();
+        this.cadastraUsuario();
+    }
+
+    acessarPaginaDeCadatro(){
+        this.dadoQueSejaInseridoLoginESenhaValido();
+        cy.get('a[data-testid="cadastrarUsuarios"]').click();
+    }
+
+    cadastraUsuario(){
+        const nome = faker.person.firstName();
+
+        cy.get('input[data-testid="nome"]').type("Thiago Teste");
+        cy.get('input[data-testid="email"]').type(nome+"@gdot.com");
+        cy.get('input[data-testid="password"]').type("12345");
+        cy.get('button[data-testid="cadastrarUsuario"]').click();
+    }
+
+    verificarCriaçãoDeUsuarios(){
+        cy.get('.table').should('be.visible');
     }
 }
 
